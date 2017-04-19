09c6784f8b7671e18901786e1fbed15b1f21f696
Rodrigo da Rosa Elesbão <elesbao@f1solucoes.com.br>
2017-04-19 17:35:31 -0300

    Added tmux config file

--- install.sh
+++ install.sh
@@ -4,7 +4,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 rm -rf $HOME/.vim 2> /dev/null
 rm -f $HOME/.vimrc 2> /dev/null
+rm -f $HOME/.tmux.conf 2> /dev/null
 mkdir -p $HOME/.vim/bundle
 ln -s $DIR/vimrc $HOME/.vimrc
+ln -s $DIR/tmux $HOME/.tmux.conf
 git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
